3d4d654aab7c4fa287a05098090ad0563628f018
solrosh <juanjose2020@hotmail.es>
2025-06-12 13:28:07 +0200

    first commit

---
+++ src/com/objetos/Prueba.java
@@ -0,0 +1,9 @@
+package com.objetos;
+
+public class Prueba {
+    // Esta clase no se va a ejecutar
+    // Creamos un metodo para saludar
+    public static void saludar(){
+        System.out.println("Saludando desde Prueba");
+    }
+}
